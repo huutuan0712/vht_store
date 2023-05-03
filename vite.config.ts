53bda3ea1d1d2a94b498c184463f5f46dc498039
huutuan0712 <tuan071220@gmail.com>
2023-05-03 22:07:06 +0700

    deploy

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {vuetify} from "./src/plugins/vuetify";
+
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+      vuetify()
+  ],
+})
